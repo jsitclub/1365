5619f7efdc308b27d9c17c0c6ea592fcf31a8dd9
jsitclub <60156470+jsitclub@users.noreply.github.com>
2020-01-29 00:27:31 +0900

    Create parser.py

---
+++ parser.py
@@ -0,0 +1,17 @@
+f=open("code.txt","r",encoding='UTF8')
+
+a=f.readlines()
+f.close()
+
+for string in a:
+    t=string[string.index(":")+2:]
+    t=t.replace('code:','"code":')
+    t=t.replace('codeNm:','"codeNm":')
+    t=t.replace('upperCode:','"upperCode":')
+    t=t.replace('engnSe:','"engnSe":')
+
+    exec("d="+t)
+
+    print(d["code"])
+    
+    
